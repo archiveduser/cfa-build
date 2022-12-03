69ef053fad30edb86dad9ba3e72dc21281d0e398
kamino <kamino@imea.me>
2022-12-03 23:51:43 +0800

    change package name

--- modify.sh
+++ modify.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sed -i 's/const defaultPersistOverride = `{"dns":{"enable": false}, "redir-port": 0, "tproxy-port": 0}`/const defaultPersistOverride = `{}`/g' ./cfa/core/src/main/golang/native/config/override.go
 sed -i 's/create("foss")/create("kamino")/g' ./cfa/build.gradle.kts
+sed -i 's/.foss/.kamino/g' ./cfa/build.gradle.kts
 sed -i 's/create("foss")/create("kamino")/g' ./cfa/core/build.gradle.kts
